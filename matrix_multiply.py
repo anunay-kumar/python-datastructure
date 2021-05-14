b9ce31a1afc216df81e4a1a0382500811a21a662
Anunay Kumar <kumaranunay123@gmail.com>
2021-05-14 17:35:33 +0530

    Update matrix_multiply.py

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -1,6 +1,10 @@
+import numpy
+
 X = [[1,2,7],[4,5,6],[7,8,9]]
 Y = [[10,11,7,1],[13,14,8,2],[16,17,9,3]]
-R = [[0,0],[0,0],[0,0]]
+R = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+print("Calculating matrix multiplication using logic:")
 
 for i in range(len(X)):
     print(X[i])
@@ -14,21 +18,14 @@ print('-' * 10)
 print('=')
 
 for i in range(len(X)):
-    for j in range(len(X[i])):
+    for j in range(len(Y[0])):
         for k in range(len(Y)):
-            temp = []
-            print('-----')
-            for m in range(len(Y[k])):
-                temp.append(Y[k][m])
-            print(temp)
-                
-                                
-        
-    '''
-        for k in range(len(Y)):
-            print(X[i][k],'*',Y[k][j])
-            sum = X[i][k] * Y[k][j] + sum
-        print(sum)
-        sum = 0
-            
-     '''
+            R[i][j] = R[i][j] + X[i][k] * Y[k][j]
+            #print(X[i][k],'*',Y[k][j],'=',R[i][j])
+    #print('---')
+
+for r in range(len(R)):
+    print(R[r])
+    
+print("\nVerify matrix multiplication using numpy:")
+print(numpy.dot(X,Y))
